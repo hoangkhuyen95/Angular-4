68b8d422cd299b853617eea3a23f4cc197a0bbdc
hoangkhuyen95 <hoangkhuyentran@gmail.com>
2017-10-23 23:38:46 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WebComponentComponent } from './web-component/web-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebComponentComponent
   ],
   imports: [
     BrowserModule

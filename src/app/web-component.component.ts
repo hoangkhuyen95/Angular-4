54aa1581ea34a100b6cf652110a85535d70b1c36
hoangkhuyen95 <hoangkhuyentran@gmail.com>
2017-10-23 17:02:13 +0700

    Add files via upload

---
+++ src/app/web-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-component',
+  templateUrl: './web-component.component.html',
+  styleUrls: ['./web-component.component.css']
+})
+export class WebComponentComponent  {
+
+
+}
